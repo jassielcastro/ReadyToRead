561a0b73bdad806d4decd24feea62bd766490aeb
Jassiel Castro Melgoza <jassmel05@gmail.com>
2021-09-21 14:24:06 -0500

    Adding Qualifiers as Annotations Module

---
+++ annotation/src/main/java/com/ajcm/annotation/Qualifiers.kt
@@ -0,0 +1,12 @@
+package com.ajcm.annotation
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseUrl
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ApiToken
+
