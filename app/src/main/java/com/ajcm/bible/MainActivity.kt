21ec455c6f567a40136f12ddc39b545601517068
Jassiel Castro Melgoza <jassmel05@gmail.com>
2021-09-22 00:21:22 -0500

    Jetpack compose configured in the project

--- app/src/main/java/com/ajcm/bible/MainActivity.kt
+++ app/src/main/java/com/ajcm/bible/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.ajcm.bible
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,6 +15,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Content()
+        }
     }
+
+    @Preview
+    @Composable
+    private fun Content() {
+        MaterialTheme {
+            TopAppBar(title = { Text(text = "My Bible") })
+        }
+    }
+
 }
