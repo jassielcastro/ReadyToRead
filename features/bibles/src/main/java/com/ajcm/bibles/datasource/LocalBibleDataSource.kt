9cfa18b81f1d24458df78507a76bd8b2c7e66bdd
Alan Jassiel Castro <alan.castro@kavak.com>
2022-02-11 09:08:01 -0600

    Annotations for Dispatchers (Room requests)

--- features/bibles/src/main/java/com/ajcm/bibles/datasource/LocalBibleDataSource.kt
+++ features/bibles/src/main/java/com/ajcm/bibles/datasource/LocalBibleDataSource.kt
@@ -2,33 +2,74 @@ package com.ajcm.bibles.datasource
 
 import com.ajcm.bibles.database.BibleDAO
 import com.ajcm.bibles.database.model.BibleDTO
+import com.ajcm.common.annotation.IoDispatcher
+import com.ajcm.common.annotation.MainScope
 import com.ajcm.data.datasource.ILocalBibleDataSource
 import com.ajcm.data.mapper.BaseMapper
 import com.ajcm.domain.entity.Bible
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class LocalBibleDataSource @Inject constructor(
     private val bibleDAO: BibleDAO,
-    private val bibleMapper: BaseMapper<Bible, BibleDTO>
+    private val bibleMapper: BaseMapper<Bible, BibleDTO>,
+    @MainScope
+    private val mainScope: CoroutineScope,
+    @IoDispatcher
+    private val ioDispatcher: CoroutineDispatcher
 ) : ILocalBibleDataSource {
 
     override suspend fun saveBibles(bibleList: List<Bible>) {
-        bibleDAO.insertBibles(bibleList.map { bibleMapper.to(it) })
+        mainScope.launch {
+            withContext(ioDispatcher) {
+                bibleDAO.insertBibles(bibleList.map { bibleMapper.to(it) })
+            }
+        }
     }
 
     override suspend fun saveBible(bible: Bible) {
-        bibleDAO.insertOrUpdateBible(bibleMapper.to(bible))
+        mainScope.launch {
+            withContext(ioDispatcher) {
+                bibleDAO.insertOrUpdateBible(bibleMapper.to(bible))
+            }
+        }
     }
 
     override suspend fun getBibles(): List<Bible> {
-        return bibleDAO.getAllBibles().map { bibleMapper.from(it) }
+        return suspendCoroutine { continuation ->
+            mainScope.launch {
+                val bibles = withContext(ioDispatcher) {
+                    bibleDAO.getAllBibles().map { bibleMapper.from(it) }
+                }
+                continuation.resume(bibles)
+            }
+        }
     }
 
     override suspend fun getFavouriteBibles(): List<Bible> {
-        return bibleDAO.getFavouriteBibles().map { bibleMapper.from(it) }
+        return suspendCoroutine { continuation ->
+            mainScope.launch {
+                val bibles = withContext(ioDispatcher) {
+                    bibleDAO.getFavouriteBibles().map { bibleMapper.from(it) }
+                }
+                continuation.resume(bibles)
+            }
+        }
     }
 
     override suspend fun getBible(bibleId: String): Bible {
-        return bibleMapper.from(bibleDAO.getBible(bibleId))
+        return suspendCoroutine { continuation ->
+            mainScope.launch {
+                val bible = withContext(ioDispatcher) {
+                    bibleMapper.from(bibleDAO.getBible(bibleId))
+                }
+                continuation.resume(bible)
+            }
+        }
     }
 }
--- features/book/src/main/java/com/ajcm/book/datasource/LocalBookDataSource.kt
+++ features/book/src/main/java/com/ajcm/book/datasource/LocalBookDataSource.kt
@@ -2,30 +2,64 @@ package com.ajcm.book.datasource
 
 import com.ajcm.book.database.BookDAO
 import com.ajcm.book.database.model.BookDTO
+import com.ajcm.common.annotation.IoDispatcher
+import com.ajcm.common.annotation.MainScope
 import com.ajcm.data.datasource.ILocalBookDataSource
 import com.ajcm.data.mapper.BaseMapper
 import com.ajcm.domain.entity.Book
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class LocalBookDataSource @Inject constructor(
     private val bookDAO: BookDAO,
-    private val bookMapper: BaseMapper<Book, BookDTO>
+    private val bookMapper: BaseMapper<Book, BookDTO>,
+    @MainScope
+    private val mainScope: CoroutineScope,
+    @IoDispatcher
+    private val ioDispatcher: CoroutineDispatcher
 ) : ILocalBookDataSource {
 
     override suspend fun saveBooks(books: List<Book>) {
-        bookDAO.insertBooks(books.map { bookMapper.to(it) })
+        mainScope.launch {
+            withContext(ioDispatcher) {
+                bookDAO.insertBooks(books.map { bookMapper.to(it) })
+            }
+        }
     }
 
     override suspend fun saveBook(book: Book) {
-        bookDAO.insertOrUpdateBook(bookMapper.to(book))
+        mainScope.launch {
+            withContext(ioDispatcher) {
+                bookDAO.insertOrUpdateBook(bookMapper.to(book))
+            }
+        }
     }
 
     override suspend fun getBooks(bibleId: String): List<Book> {
-        return bookDAO.getAllBooksByBible(bibleId).map { bookMapper.from(it) }
+        return suspendCoroutine { continuation ->
+            mainScope.launch {
+                val books = withContext(ioDispatcher) {
+                    bookDAO.getAllBooksByBible(bibleId).map { bookMapper.from(it) }
+                }
+                continuation.resume(books)
+            }
+        }
     }
 
     override suspend fun getBook(bibleId: String, bookId: String): Book {
-        return bookMapper.from(bookDAO.getBook(bookId, bibleId))
+        return suspendCoroutine { continuation ->
+            mainScope.launch {
+                val book = withContext(ioDispatcher) {
+                    bookMapper.from(bookDAO.getBook(bookId, bibleId))
+                }
+                continuation.resume(book)
+            }
+        }
     }
 
 }
