ef190c363b52d50994a00a41e2b6d28258e0162a
Alan Jassiel Castro <alan.castro@kavak.com>
2022-02-11 13:40:29 -0600

    Unify libs version

--- app/src/main/java/com/ajcm/bible/ui/navigation/Navigation.kt
+++ app/src/main/java/com/ajcm/bible/ui/navigation/Navigation.kt
@@ -2,11 +2,13 @@ package com.ajcm.bible.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.ajcm.splash.ui.SplashScreen
 import com.ajcm.design.NavigationItems
+import com.ajcm.splash.viewModel.SplashViewModel
 
 @Composable
 fun Navigation(navController: NavHostController, modifier: Modifier = Modifier) {
@@ -16,7 +18,8 @@ fun Navigation(navController: NavHostController, modifier: Modifier = Modifier) 
         startDestination = NavigationItems.SPLASH_SCREEN
     ) {
         composable(NavigationItems.SPLASH_SCREEN) {
-            SplashScreen(navController)
+            val splashViewModel: SplashViewModel = hiltViewModel()
+            SplashScreen(navController, splashViewModel)
         }
     }
 }
