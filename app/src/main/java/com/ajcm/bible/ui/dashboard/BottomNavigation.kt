a9d329e037e6fdec6aaba27d66314f896bef3cfe
Alan Jassiel Castro Melgoza <alanme@backbase.com>
2022-11-13 14:36:15 -0600

    fix: animation of BottomNavigation items

--- app/src/main/java/com/ajcm/bible/ui/dashboard/BottomNavigation.kt
+++ app/src/main/java/com/ajcm/bible/ui/dashboard/BottomNavigation.kt
@@ -10,7 +10,8 @@ import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -31,18 +32,18 @@ fun BottomNavigationBar(navController: NavController, showBottomBar: Boolean) {
         ) {
             val navBackStackEntry by navController.currentBackStackEntryAsState()
             val currentRoute = navBackStackEntry?.destination?.route?.cleanRoute()
+            val shapeColor = MaterialBibleTheme.colors.green.copy(alpha = 0.7f)
+            val unselectedColor = MaterialBibleTheme.colors.black.copy(alpha = 0.35f)
 
             bottomNavigationItems.forEach { item ->
                 val isSelected = currentRoute == item.route.cleanRoute()
                 val heightMultiplier: Float by animateFloatAsState(if (isSelected) 0.65f else 0f)
-                val alpha: Float by animateFloatAsState(if (isSelected) 0.7f else 0f)
-                val shapeColor = MaterialBibleTheme.colors.green.copy(alpha = alpha)
                 val icon = if (isSelected) item.iconSelected else item.iconNormal
 
                 BottomNavigationItem(
-                    icon = { Icon(painterResource(id = icon), contentDescription = "") },
+                    icon = { Icon(imageVector = ImageVector.vectorResource(id = icon), contentDescription = "") },
                     selectedContentColor = MaterialBibleTheme.colors.white,
-                    unselectedContentColor = MaterialBibleTheme.colors.black.copy(alpha = 0.35f),
+                    unselectedContentColor = unselectedColor,
                     alwaysShowLabel = false,
                     selected = isSelected,
                     modifier = Modifier
