01a9feaeb790097c4be8ba143f61b412755c66c5
Alan Jassiel Castro Melgoza <alanme@backbase.com>
2022-11-20 01:46:05 -0600

    feature: add new splash color

--- app/src/main/java/com/ajcm/bible/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/ajcm/bible/ui/splash/SplashScreen.kt
@@ -1,5 +1,7 @@
 package com.ajcm.bible.ui.splash.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
@@ -9,7 +11,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.ajcm.bible.R
 import com.ajcm.bible.ui.error.ErrorType
 import com.ajcm.bible.ui.navigation.StartNavigationActions
@@ -17,6 +24,7 @@ import com.ajcm.bible.ui.splash.SplashViewModel
 import com.ajcm.design.common.State
 import com.ajcm.design.component.MediumSpacer
 import com.ajcm.design.component.SmallSpacer
+import com.ajcm.design.screen.BibleScreen
 import com.ajcm.design.theme.MaterialBibleTheme
 
 @Composable
@@ -43,35 +51,59 @@ fun SplashScreen(
 
 @Composable
 fun SplashContent() {
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize()
+            .background(MaterialBibleTheme.colors.green),
+        contentAlignment = Alignment.BottomCenter
     ) {
-        Text(
-            text = stringResource(id = R.string.app_name),
-            color = MaterialBibleTheme.colors.black,
-            style = MaterialBibleTheme.typography.h1
-        )
-
-        MediumSpacer()
-
-        LinearProgressIndicator(
+        Image(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_waves),
+            contentDescription = "",
+            contentScale = ContentScale.Inside,
             modifier = Modifier
-                .fillMaxWidth(0.7f)
-                .clip(MaterialBibleTheme.shapes.shapeSmall)
-                .height(MaterialBibleTheme.dimensions.small),
-            backgroundColor = MaterialBibleTheme.colors.greenLight,
-            color = MaterialBibleTheme.colors.green
+                .fillMaxWidth()
+                .rotate(0f)
         )
 
-        SmallSpacer()
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                color = MaterialBibleTheme.colors.white,
+                style = MaterialBibleTheme.typography.h1
+            )
 
-        Text(
-            text = stringResource(id = R.string.load_content),
-            color = MaterialBibleTheme.colors.black,
-            style = MaterialBibleTheme.typography.caption
-        )
+            MediumSpacer()
+
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth(0.7f)
+                    .clip(MaterialBibleTheme.shapes.shapeSmall)
+                    .height(MaterialBibleTheme.dimensions.small),
+                backgroundColor = MaterialBibleTheme.colors.white,
+                color = MaterialBibleTheme.colors.greenLight
+            )
+
+            SmallSpacer()
+
+            Text(
+                text = stringResource(id = R.string.load_content),
+                color = MaterialBibleTheme.colors.white,
+                style = MaterialBibleTheme.typography.caption
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewComponent() {
+    BibleScreen {
+        SplashContent()
     }
 }
